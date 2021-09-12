4d6e19262bf00906c6ac30c041f1bc8aa5aff9cb
Giovanni <gbertao96@gmail.com>
2021-09-12 11:55:31 -0300

    Functions created, UI freezes

--- app/src/main/java/com/giovanni/example/primefinderui/MainActivity.java
+++ app/src/main/java/com/giovanni/example/primefinderui/MainActivity.java
@@ -3,12 +3,47 @@ package com.giovanni.example.primefinderui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText)findViewById(R.id.etInput);
+        tv = (TextView)findViewById(R.id.tvResult);
+    }
+
+    public void DeployCheckPrime (View v) {
+        long lgnNum;
+        String number = et.getText().toString();
+        tv.setText("Checking Wait");
+        try {
+            lgnNum = Long.parseLong(number);
+        } catch (Exception e) {
+            tv.setText("Invalid Number");
+            return;
+        }
+        tv.setText(isPrime(lgnNum) ? "Is Prime" : "Is not Prime");
+    }
+
+    private boolean isPrime (long n) {
+        if (n %2 == 0)
+            return false;
+        if (n%3 == 0)
+            return false;
+
+        long last = (long)Math.sqrt(n);
+        for (long i = 5; i < last; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 }
